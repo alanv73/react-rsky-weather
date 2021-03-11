346aaf3c9db1e1c59568f2e5b7bf676d84d692b3
alanv73@gmail.com <alanv73@gmail.com>
2021-03-11 12:59:52 -0500

    move styles to file and cleanup unused variables

--- src/DailyWeather.js
+++ src/DailyWeather.js
@@ -1,69 +1,16 @@
 import React, { useEffect, useContext } from 'react';
 import { DailyContext } from './contexts/weatherContext';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
-import { makeStyles } from '@material-ui/core';
 import TwoColTable from './TwoColTable';
 import FieldDropDown from './FieldDropDown';
 import { DAILY_FIELDS } from './constants';
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        // width: 600,
-        margin: '30px 30px',
-        
-    },
-    chart: {
-        padding: '10px 0px',
-        background: 'rgba(0,0,0,0.6)',
-        borderRadius: 10,
-        width: 525,
-        // margin: '0px 20px',
-        "& select": {
-            display: 'block',
-            margin: '5px auto',
-            fontFamily: 'Montserrat',
-            fontSize: 18,
-            border: 'none',
-            borderRadius: 3,
-            background: 'transparent',//'rgba(255,255,255,0.6)',
-            color: 'white',
-            outline: 'none',
-        },
-        "& option": {
-            background: 'rgba(0,0,0,0.7)',
-        },
-        "& svg": {
-            padding: 10,
-        }
-    },
-    tableWrapper: {
-        width: 500,
-        background: 'rgba(0,0,0,0.6)',
-        borderRadius: 10,
-        padding: 12.5,
-        margin: '20px 0px'
-
-    },
-    table: {
-        fontFamily: 'Montserrat',
-        height: '250px',
-        width: '50%',
-        overflowY: 'scroll',
-        margin: '20px auto',
-        color: 'white',
-    },
-}));
+import useStyles from './styles/DailyWeatherStyles';
 
 function DailyWeather() {
     const classes = useStyles();
     const {
-        dailyData,
         currentDate, 
-        setCurrentDate, 
         getDailyDataState, 
-        getDailyParamArray, 
-        getMinParam, 
-        getMaxParam,
         getDayChartData,
         dChartField,
         handleDayChartChange
