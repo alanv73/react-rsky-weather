8d3a1825e0e56154590759b21d896a0a27699c27
alanv73@gmail.com <alanv73@gmail.com>
2021-03-11 13:03:19 -0500

    move styles to file and cleanup unused imports

--- src/CurrentWeatherCard.js
+++ src/CurrentWeatherCard.js
@@ -5,97 +5,13 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
-import { Divider, makeStyles } from '@material-ui/core';
+import { Divider } from '@material-ui/core';
 import CardHeader from '@material-ui/core/CardHeader';
 import { withStyles } from '@material-ui/core/styles';
-import { green, blue } from '@material-ui/core/colors';
-import { fade } from '@material-ui/core/styles/colorManipulator';
+import { blue } from '@material-ui/core/colors';
 import Spinner from './assets/spinner.gif';
 import arrow from './assets/arrow_wht.png';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    borderRadius: 12,
-    width: 525,
-    height: 300,
-    textAlign: 'center',
-    margin: '30px auto',
-    overflow: 'auto',
-    background: 'rgba(0,0,0, 0.6)',
-    color: 'white',
-  },
-  divider: {
-    background: 'white',
-  },
-  header: {
-    textAlign: 'center',
-    spacing: 10,
-    // paddingBottom: 8,
-  },
-  content: {
-    padding: '24px 24px',
-  },
-  temp: {
-      float: 'left',
-      marginRight: 20,
-  },
-  list: {
-    // background: 'rgba(196,196,196, 0.3)',
-    // borderRadius: theme.spacing(2),
-    padding: '0px 0px',
-    paddingLeft: 20,
-    textAlign: 'left',
-    display: 'flex',
-    flexDirection: 'column',
-    flexWrap: 'wrap',
-    justifyContent: 'flex-start',
-    height: '6rem',
-    overflow: 'auto',
-    "& p": {
-        // flexBasis: '40%',
-        // background: 'rgba(196,196,196, 0.3)',
-        // borderRadius: theme.spacing(2),
-        // borderBottom: '1px solid black',
-        padding: '0px 10px',
-        // marginBottom: 5,
-
-    }
-  },
-  wind: {
-    position: 'absolute',
-    left: '35%',
-    top: '14%',
-    width: 140,
-    height: 70,
-  },
-  gust: {
-    // fontFamily: 'Montserrat',
-    // color: '#fff',
-    opacity: 0.87,
-    fontWeight: 500,
-    fontSize: 12,
-    position: 'absolute',
-    bottom: -15,
-    left: '25%',
-  },
-  arrow: {
-    width: 35,
-    height: 50,
-    position: 'absolute',
-    left: 'calc(50% - 17px)',
-    top: 'calc(50% - 22px)',
-  },
-  button: {
-    margin: theme.spacing(1),
-    paddingLeft: theme.spacing(1.5),
-    paddingRight: theme.spacing(1.5),
-    borderRadius: theme.spacing(2),
-  },
-  action: {
-    display: 'flex',
-    justifyContent: 'space-around',
-  },
-}));
+import useStyles from './styles/CurrentWeatherCardStyles';
 
 function CurrentWeatherCard() {
     const classes = useStyles();
