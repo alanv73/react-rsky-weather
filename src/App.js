b639ae3e2d9fefa7cde6b89554a43437236a9ed8
alanv73@gmail.com <alanv73@gmail.com>
2021-03-11 14:06:52 -0500

    add a theme with a font

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import { WeatherProvider } from './contexts/weatherContext';
+import { ThemeProvider } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import CurrentWeatherCard from './CurrentWeatherCard';
 import DailyWeather from './DailyWeather';
 import DaySummaryList from './DaySummaryList';
+import theme from './styles/theme';
 import './App.css';
 
 function App() {
   document.title = "Raspberry Sky";
   return (
     <div className="App">
-      <WeatherProvider>
-        <Grid direction="column" >
-          <CurrentWeatherCard />
-          <DailyWeather />
-        </Grid>
-        <DaySummaryList />
-      </WeatherProvider>
+      <ThemeProvider theme={theme}>
+        <WeatherProvider>
+          <Grid direction="column" >
+            <CurrentWeatherCard />
+            <DailyWeather />
+          </Grid>
+          <DaySummaryList />
+        </WeatherProvider>
+      </ThemeProvider>
     </div>
   );
 }
