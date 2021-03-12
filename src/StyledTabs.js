453bf6ab253d34b6e717cc1167f87e2db041fe07
alanv73@gmail.com <alanv73@gmail.com>
2021-03-12 16:20:40 -0500

    combine tab components

--- src/StyledTabs.js
+++ src/StyledTabs.js
@@ -1,7 +1,11 @@
 import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import { withStyles } from '@material-ui/core/styles';
 
-const StyledTabs = withStyles({
+export const StyledTabs = withStyles({
+  root: {
+    minHeight: 0,
+  },
   indicator: {
     display: 'flex',
     justifyContent: 'center',
@@ -19,4 +23,15 @@ const StyledTabs = withStyles({
     />
 );
 
-export default StyledTabs;
+export const StyledTab = withStyles((theme) => ({
+  root: {
+    textTransform: 'none',
+    fontWeight: theme.typography.fontWeightRegular,
+    fontSize: theme.typography.pxToRem(15),
+    marginRight: theme.spacing(1),
+    minHeight: 0,
+    '&:focus': {
+      opacity: 1,
+    },
+  },
+}))((props) => <Tab {...props} />);
